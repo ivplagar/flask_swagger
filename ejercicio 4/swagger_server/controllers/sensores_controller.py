c631cc23f3ec6ead326aa86daaa7f412db11cdc4
Ivan Pla Garcia <ivan.pla.1992@gmail.com>
2022-12-16 19:34:35 +0100

    .

--- ejercicio 4/swagger_server/controllers/sensores_controller.py
+++ ejercicio 4/swagger_server/controllers/sensores_controller.py
@@ -6,6 +6,23 @@ from swagger_server.models.message import Message  # noqa: E501
 from swagger_server import util
 
 
+def get_historical_measures_by_sensor_and_type(sensor, start, end):  # noqa: E501
+    """Obtener mediciones históricas del sensor en un rango de fechas
+
+    Recuperar las mediciones de un sensor en un rango de fechas específico # noqa: E501
+
+    :param sensor: ID del sensor
+    :type sensor: str
+    :param start: Fecha de inicio del rango (en formato yyyy-mm-dd)
+    :type start: str
+    :param end: Fecha de fin del rango (en formato yyyy-mm-dd)
+    :type end: str
+
+    :rtype: List[Meassure]
+    """
+    return 'do some magic!'
+
+
 def get_last_meassure_by_sensor(sensor):  # noqa: E501
     """Obtener la última medición del sensor
 
--- ejercicio 4/swagger_server/test/test_sensores_controller.py
+++ ejercicio 4/swagger_server/test/test_sensores_controller.py
@@ -13,6 +13,18 @@ from swagger_server.test import BaseTestCase
 class TestSensoresController(BaseTestCase):
     """SensoresController integration test stubs"""
 
+    def test_get_historical_measures_by_sensor_and_type(self):
+        """Test case for get_historical_measures_by_sensor_and_type
+
+        Obtener mediciones históricas del sensor en un rango de fechas
+        """
+        response = self.client.open(
+            '/v2/getHistoricalMeasuresBySensorAndType/{sensor}/{start}/{end}'.format(sensor='sensor_example', start='start_example', end='end_example'),
+            method='GET',
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
     def test_get_last_meassure_by_sensor(self):
         """Test case for get_last_meassure_by_sensor
 
